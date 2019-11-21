c276ffd4a185645c9e78595888e369dd449deb87
hzxleo4 <hzxleo4@163.com>
2019-11-21 13:18:24 +0800

    Bug fixed

--- Week-1/Userfilter.py
+++ Week-1/Userfilter.py
@@ -1,6 +1,9 @@
 import re
 with open("name.txt","r") as file:
     str = file.read()
-    a = re.findall(r'\([0-9]*?\)[a-z][a-zA-Z_-]{4,19}',str)
-    print(len(a))
+    #str = "(1)attttttttttttt(2)afiahaaa(3)aiufdhaaaaaaaaaaaaaaaaa(4)sfaefefastsssssssststst"
+    a = re.findall(r'[0-9]*?\)[a-z]{1}[a-zA-Z_-]{4,19}\(',str)
+    b = re.findall(r'\([0-9]*?\)[a-z]{1}[a-zA-Z_-]{4,19}$',str)
+    print(len(a)+len(b))
+    #print(a)
     file.close()
